136f28f649d8557538a23c0f6ecb9b9c63e1fb46
Dexter Shepherd <dextershepherd@hotmail.co.uk>
2023-11-05 18:00:02 +0000

    Update sound.py

--- code/sound.py
+++ code/sound.py
@@ -8,9 +8,11 @@ beeper = PWMOut(board.GP18, variable_frequency=True)
 while True:
     v=l.filter(l.step())
     beeper.duty_cycle = 2 ** 15
+    freq=((int(v[0]*40000)) +  (int(v[4]*40000)))//2 +1000
+    print(freq)
     try:
-        beeper.frequency = abs(int(v[0]*40000))
+        beeper.frequency = freq
     except:
-        print("error",int(v[0]*40000))
+        print("error",freq)
         beeper.frequency = 1
 
