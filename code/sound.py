a40e7d622db67a4059056c06b3480050d4fc5e29
Dexter Shepherd <dextershepherd@hotmail.co.uk>
2023-11-05 17:55:22 +0000

    Update sound.py

--- code/sound.py
+++ code/sound.py
@@ -1,16 +1,13 @@
 from listener import *
-from pwmio import PWMOut
 import board
+from pwmio import PWMOut
+
 l=listener()
 
-# Generate one period of sine wav.
-length = 10000
-
 beeper = PWMOut(board.GP18, variable_frequency=True)
-for i in range(length):
+while True:
     v=l.filter(l.step())
     beeper.duty_cycle = 2 ** 15
-    print(sine_wave[i])
     try:
         beeper.frequency = abs(int(v[0]*40000))
     except:
